abf140209363886eb6315f7916ce75baddc29521
Krish <krishnendu.dutta.dev@gmail.com>
2026-06-03 02:26:29 +0530

    solving the frontend and backend connection bug

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,7 +14,15 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" })); // req.body
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://whisper-q4qt.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
