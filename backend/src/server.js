f0aec0217d2f0c8d1ef9b4b576547434865d1089
Krish <krishnendu.dutta.dev@gmail.com>
2026-06-16 17:50:56 +0530

    Logging neccesary information on console for debugging

--- backend/src/server.js
+++ backend/src/server.js
@@ -29,6 +29,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
+console.log(ENV.NODE_ENV);
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
